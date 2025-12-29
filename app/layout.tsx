6f02d4b22702dba8249cf3b7ea1bcb2c553c70cb
Hunter Koth <hunterkoth04@gmail.com>
2025-12-28 23:11:18 -0800

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'ICG-ATS | Application Tracking System',
   description: 'Irvine Consulting Group Application Tracking System',
+  icons: {
+    icon: '/images/ICG 4x4 Transparent White.png',
+  },
 };
 
 export default function RootLayout({
